b9e0e1da741a49a13550b8d882e20046fbd985eb
Claire Bedrossian <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-12-22 00:37:26 +0000

    whitespace 1

--- main.cs
+++ main.cs
@@ -6,7 +6,8 @@ using System.Net.Http;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 
-class Program {
+class Program
+{
     public static async Task Main()
     {
         HttpClient client = new HttpClient();
@@ -14,7 +15,7 @@ class Program {
             "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
         );
 
-      // Console.WriteLine(jsonAsDictionary);
+        // Console.WriteLine(jsonAsDictionary);
         Console.WriteLine("");
         JsonNode forecastNode = JsonNode.Parse(response)!;
         // Console.WriteLine(forecastNode);
